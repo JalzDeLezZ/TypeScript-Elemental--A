17ae56ffaa0263d543e2e4d12333e28567fb9698
JalzDeLezZ <j4mes.delez@gmail.com>
2022-07-22 00:46:18 -0500

    nine

---
+++ src/03-typing.ts
@@ -0,0 +1,14 @@
+(() =>{
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+  
+  myProductName = 'ASDFASDF';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed(2);
+
+  const myProductStock = 1000;
+  const myProductStock2 = "Product 1";
+})();
+
+
---
+++ src/04.numbers.ts
@@ -0,0 +1,33 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log(productPrice);
+
+  let customerAge: number = 28;
+  customerAge = customerAge;
+
+  let productInStock: number;
+
+  console.log('productInStock', productInStock);
+
+  if (productInStock > 19) {
+    console.log('Product in stock');
+  }
+
+  let discount = parseInt('123ASD');
+  console.log('discount', discount);
+
+  if (discount > 0) {
+    console.log('Discount is greater than 0');
+  } else {
+    console.log('Discount is less than 0');
+  }
+
+  let hex = 0xff;
+  console.log('hex', hex);
+  let bin = 0b1010; //10
+  console.log('bin', bin);
+
+  const myNumber: number = 123;
+  
+})();
