afdc631ad8a2615a41df635bd4b5b868180b42bb
JalzDeLezZ <j4mes.delez@gmail.com>
2022-07-22 06:01:34 -0500

    1

---
+++ src/09-union types.ts
@@ -0,0 +1,16 @@
+(() => {
+  let userId: string | number;
+  userId = 1234;
+  let userId2: string = 'ASDFASDF';
+
+  function greeting(myText: string | number) {
+    if (typeof myText === 'string') {
+      console.log(`string  ${myText.toLocaleLowerCase()}`);
+    } else {
+      console.log(`number ${myText.toFixed()}`);
+    }
+  }
+
+  greeting(userId);
+  greeting(userId2);
+})();
